5d08cefd5187bb16e81ab8bedc1c5454e8e44446
HyeongH <89951007+HyeongH@users.noreply.github.com>
2021-09-03 12:52:43 +0900

    0903

--- src/org/opentutorials/javatutorials/eclipse/spring.java
+++ src/org/opentutorials/javatutorials/eclipse/spring.java
@@ -3,7 +3,7 @@ package org.opentutorials.javatutorials.eclipse;
 public class spring {
 
 	public static void main(String[] args) {
-		System.out.println("Hello java");
+		System.out.println("Hello java!"+"><");
 
 	}
 
