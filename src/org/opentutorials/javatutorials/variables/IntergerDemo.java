52d41e2d299318f58c0cbe795cb4497889d77a2d
HyeongH <89951007+HyeongH@users.noreply.github.com>
2021-09-03 12:40:38 +0900

    int

---
+++ src/org/opentutorials/javatutorials/variables/IntergerDemo.java
@@ -0,0 +1,18 @@
+package org.opentutorials.javatutorials.variables;
+
+public class IntergerDemo {
+
+	public static void main(String[] args) {
+		int a;
+		a=1;
+		System.out.println(a+3);
+		
+		a=4;
+		System.out.println(a+2);
+
+		
+
+
+	}
+
+}
