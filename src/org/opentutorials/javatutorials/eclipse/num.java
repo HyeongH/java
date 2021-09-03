6432a0bd0b664c9772f1c78a02d09d9756882359
HyeongH <89951007+HyeongH@users.noreply.github.com>
2021-09-03 12:47:09 +0900

    0903

--- src/org/opentutorials/javatutorials/eclipse/num.java
+++ src/org/opentutorials/javatutorials/eclipse/num.java
@@ -3,7 +3,7 @@ package org.opentutorials.javatutorials.eclipse;
 public class num {
 
 	public static void main(String[] args) {
-		System.out.println("1"+"hi");
+		System.out.println(1+6/6);
 
 	}
 
