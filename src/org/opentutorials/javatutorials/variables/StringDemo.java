99c75145f3d06b62c8082030aed00d0f840b11c9
HyeongH <89951007+HyeongH@users.noreply.github.com>
2021-09-07 12:27:53 +0900

    String

---
+++ src/org/opentutorials/javatutorials/variables/StringDemo.java
@@ -0,0 +1,22 @@
+package org.opentutorials.javatutorials.variables;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String first = "coding"; 
+		System.out.println(first+" "+"everybody");
+		
+		String hi = "Hello";
+		System.out.println(hi+" "+"java");
+		
+		String a = "hyeong";
+		String b = " h";
+		System.out.println(a+b);
+		
+		a = "B.D";
+		b = " 6.14";
+		System.out.println(a+b);
+
+	}
+
+}
