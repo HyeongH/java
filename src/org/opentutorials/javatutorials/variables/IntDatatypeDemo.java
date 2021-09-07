f9bbbc4efa3ad64579a0d7528306e0a34fdb9529
HyeongH <89951007+HyeongH@users.noreply.github.com>
2021-09-07 12:57:34 +0900

    IntDatatype

---
+++ src/org/opentutorials/javatutorials/variables/IntDatatypeDemo.java
@@ -0,0 +1,23 @@
+package org.opentutorials.javatutorials.variables;
+
+public class IntDatatypeDemo {
+
+	public static void main(String[] args) {
+		byte a = 127;
+		//byte = -128 ~ 127 
+		System.out.println(a);
+		
+		short b = -32768;
+		//short = -32768 ~ 32767
+		System.out.println(b);
+		
+		int c = -2147483648;
+		//int = -2,147,483,648 ~ 2,147,483,647
+		System.out.println(c);
+		
+		/*램 사용의 효율을 생각하며 변수 사용한다*/
+		
+		
+	}
+
+}
