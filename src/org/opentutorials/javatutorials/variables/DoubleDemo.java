8c70dd46447e6e8eb4ad1eca5c3b135e0876ff0d
HyeongH <89951007+HyeongH@users.noreply.github.com>
2021-09-07 12:11:58 +0900

    double

---
+++ src/org/opentutorials/javatutorials/variables/DoubleDemo.java
@@ -0,0 +1,16 @@
+package org.opentutorials.javatutorials.variables;
+
+public class DoubleDemo {
+
+	public static void main(String[] args) {
+		double a = 1.1; 
+		System.out.println(a+1.1);
+		 
+		a = 2.1; 
+		System.out.println(a+1.1);
+		
+		double c = 8.24;
+		System.out.println(a+c);
+	}
+
+}
